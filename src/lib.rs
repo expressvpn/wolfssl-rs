218e55b6aeabbc41a9b989da71a666af9ec2fd7f
Pang T <126134360+expressvpn-pang-t@users.noreply.github.com>
2023-03-23 15:58:23 +0800

    Wrap `wolfSSL_Init` and `wolfSSL_Cleanup`

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,47 @@
 //! the `wolfssl-sys` crate (a C passthrough crate).
 
 #![warn(missing_docs)]
+
+mod errors;
+use errors::{WolfCleanupError, WolfInitError};
+
+/// Wraps [`wolfSSL_Init`][0]
+///
+/// Note that this is also internally during initialization by
+/// [`WolfContextBuilder`].
+///
+/// [0]: https://www.wolfssl.com/documentation/manuals/wolfssl/group__TLS.html#function-wolfssl_init
+pub fn wolf_init() -> Result<(), WolfInitError> {
+    match unsafe { wolfssl_sys::wolfSSL_Init() } {
+        wolfssl_sys::WOLFSSL_SUCCESS => Ok(()),
+        wolfssl_sys::BAD_MUTEX_E => Err(WolfInitError::Mutex),
+        wolfssl_sys::WC_INIT_E => Err(WolfInitError::WolfCrypt),
+        e => panic!("Unexpected return value from `wolfSSL_Init`. Got {e}"),
+    }
+}
+
+/// Wraps [`wolfSSL_Cleanup`][0]
+///
+/// [0]: https://www.wolfssl.com/documentation/manuals/wolfssl/group__TLS.html#function-wolfssl_cleanup
+pub fn wolf_cleanup() -> Result<(), WolfCleanupError> {
+    match unsafe { wolfssl_sys::wolfSSL_Cleanup() } {
+        wolfssl_sys::WOLFSSL_SUCCESS => Ok(()),
+        wolfssl_sys::BAD_MUTEX_E => Err(WolfCleanupError::Mutex),
+        e => panic!("Unexpected return value from `wolfSSL_Cleanup. Got {e}`"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn wolf_init_test() {
+        wolf_init().unwrap();
+    }
+
+    #[test]
+    fn wolf_cleanup_test() {
+        wolf_cleanup().unwrap();
+    }
+}
