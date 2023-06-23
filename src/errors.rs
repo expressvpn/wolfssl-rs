c625e6b37293d35caaae52401e20962d971f54c1
Pang T <126134360+expressvpn-pang-t@users.noreply.github.com>
2023-06-23 11:39:44 +0800

    Add `thiserror` crate

--- src/errors.rs
+++ src/errors.rs
@@ -1,16 +1,18 @@
-/// Return error values for [`wolf_init`]
-#[derive(Debug)]
+use thiserror::Error;
+
+/// Return error values for [`crate::wolf_init`]
+#[derive(Error, Debug)]
 pub enum WolfInitError {
-    /// Corresponds with `BAD_MUTEX_E`
+    #[error("BAD_MUTEX_E")]
     Mutex,
-    /// Corresponds with `WC_INIT_E`
+    #[error("WC_INIT_E")]
     WolfCrypt,
 }
 
-/// Return error values for [`wolf_cleanup`]
-#[derive(Debug)]
+/// Return error values for [`crate::wolf_cleanup`]
+#[derive(Error, Debug)]
 pub enum WolfCleanupError {
-    /// Corresponds with `BAD_MUTEX_E`
+    #[error("BAD_MUTEX_E")]
     Mutex,
 }
 
@@ -19,32 +21,34 @@ pub enum WolfCleanupError {
 ///
 /// [0]: https://www.wolfssl.com/documentation/manuals/wolfssl/group__CertsKeys.html#function-wolfssl_ctx_load_verify_buffer
 /// [1]: https://www.wolfssl.com/documentation/manuals/wolfssl/group__CertsKeys.html#function-wolfssl_ctx_load_verify_locations
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum LoadRootCertificateError {
-    /// `SSL_FAILURE`
+    #[error("SSL_FAILURE")]
     Failure,
-    /// `SSL_BAD_FILETYPE`
+    #[error("SSL_BAD_FILETYPE")]
     BadFiletype,
-    /// `SSL_BAD_FILE`
+    #[error("SSL_BAD_FILE")]
     BadFile,
-    /// `MEMORY_E`
+    #[error("MEMORY_E")]
     Memory,
-    /// `ASN_INPUT_E`
+    #[error("ASN_INPUT_E")]
     AsnInput,
-    /// `ASN_BEFORE_DATE_E`
+    #[error("ASN_BEFORE_DATE_E")]
     AsnBeforeDate,
-    /// `ASN_AFTER_DATE_E`
+    #[error("ASN_AFTER_DATE_E")]
     AsnAfterDate,
-    /// `BUFFER_E`
+    #[error("BUFFER_E")]
     Buffer,
-    /// `BAD_PATH_ERROR`
+    #[error("BAD_PATH_ERROR")]
     Path,
-    /// Error values outside of what was documented
+    #[error("Unknown: {0}")]
     Other(i64),
 }
 
-impl From<i32> for LoadRootCertificateError {
-    fn from(value: std::os::raw::c_int) -> Self {
+use std::os::raw::c_int;
+
+impl From<c_int> for LoadRootCertificateError {
+    fn from(value: c_int) -> Self {
         match value {
             wolfssl_sys::WOLFSSL_BAD_FILETYPE => Self::BadFiletype,
             wolfssl_sys::WOLFSSL_BAD_FILE => Self::BadFile,
@@ -55,7 +59,7 @@ impl From<i32> for LoadRootCertificateError {
             wolfssl_sys::ASN_AFTER_DATE_E => Self::AsnAfterDate,
             wolfssl_sys::ASN_BEFORE_DATE_E => Self::AsnBeforeDate,
             wolfssl_sys::BAD_PATH_ERROR => Self::Path,
-            e => Self::Other(e as i64), // e => panic!("Undocumented return value: got {e}"),
+            e => Self::Other(e as i64),
         }
     }
 }
