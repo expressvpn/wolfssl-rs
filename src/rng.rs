78070c142a9fee86f825cd28385476884257a9e6
Mariappan Ramasamy <142989797+expressvpn-mariappan-r@users.noreply.github.com>
2023-09-12 12:05:24 +0800

    Add safety comment for apis in rng.rs
    - wc_InitRng
    - wc_RNG_GenerateBlock
    - wc_FreeRng

--- src/rng.rs
+++ src/rng.rs
@@ -1,4 +1,3 @@
-#![allow(clippy::undocumented_unsafe_blocks)]
 use crate::error::{Error, Result};
 
 /// Provides a way to extract random values from WolfSSL.
@@ -8,8 +7,19 @@ impl Random {
     /// Initializes a [`Random`] object.
     pub fn new() -> Result<Random> {
         let mut rng = std::mem::MaybeUninit::<wolfssl_sys::WC_RNG>::uninit();
+        // SAFETY:
+        // [`wc_InitRng()`][0] ([also][1]) is documented to receive [`WC_RNG`] as input to initialise seed and key cipher.
+        // The corresponding memory is deallocated, when dropping this structure
+        //
+        // [0]: https://www.wolfssl.com/doxygen/group__Random.html#ga1a87307fac65d3c2a47ffb743020f83c
+        // [1]: https://www.wolfssl.com/documentation/manuals/wolfssl/group__Random.html#function-wc_initrng
         match unsafe { wolfssl_sys::wc_InitRng(rng.as_mut_ptr()) } {
             0 => {
+                // SAFETY:
+                // Based on the documentation from [`wc_InitRng()`][0], `rng` will be initialised successfully
+                // if the return code is `0`. So can safely call `assume_init` here
+                //
+                // [0]: https://www.wolfssl.com/doxygen/group__Random.html#ga1a87307fac65d3c2a47ffb743020f83c
                 let rng = unsafe { rng.assume_init() };
                 Ok(Random(rng))
             }
@@ -20,6 +30,12 @@ impl Random {
     /// Generates a random u64. Advances the internal state of the RNG.
     pub fn random_u64(&mut self) -> Result<u64> {
         let mut buf = [0u8; 8];
+        // SAFETY:
+        // The following invariants required by [`wc_RNG_GenerateBlock()`][0] is satisfied:
+        //      - first argument `&rng` is mutable reference to properly initialised random number generator
+        //      - second argment has valid mutable buffer with proper size corresponding to the size `sz`
+        //
+        // [0]: https://www.wolfssl.com/doxygen/group__Random.html#ga9a289fb3f58f4a5f7e15c2b5a1b0d7c6
         match unsafe {
             wolfssl_sys::wc_RNG_GenerateBlock(&mut self.0 as *mut _, buf.as_mut_ptr(), 8)
         } {
@@ -31,6 +47,11 @@ impl Random {
 
 impl Drop for Random {
     fn drop(&mut self) {
+        // SAFETY:
+        // [`wc_FreeRng()`][0] receives properly initialised random number generator [`WC_RNG`] as input to securely free drgb.
+        // The pointer used as argument is created by `wc_InitRng` api call in new() constructor.
+        //
+        // [0]: https://www.wolfssl.com/doxygen/group__Random.html#ga72ffd8b507b3a895af8a6e9996caba86
         let res = unsafe { wolfssl_sys::wc_FreeRng(&mut self.0 as *mut _) };
 
         if res != 0 {
