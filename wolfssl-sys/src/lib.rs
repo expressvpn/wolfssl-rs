cee29d5d012620b4c1630c0a1ad6a1a2907d1859
Thomas Yau <thomas.yau@kape.com>
2024-11-12 12:18:05 +0800

    CVPN-1445 Add ML-KEM groups
    Add relevant tests and different ML-KEM groups to be used in the Lightway.

--- wolfssl-sys/src/lib.rs
+++ wolfssl-sys/src/lib.rs
@@ -20,6 +20,7 @@ mod tests {
     #[test]
     #[cfg(feature = "postquantum")]
     fn test_post_quantum_available() {
+        // Test if original Kyber is availble
         unsafe {
             // Init WolfSSL
             let res = wolfSSL_Init();
@@ -37,7 +38,29 @@ mod tests {
             // Enable Kyber
             let res = wolfSSL_UseKeyShare(ssl, WOLFSSL_P521_KYBER_LEVEL5.try_into().unwrap());
 
-            // Check that Kyber was enabled
+            // Check that ML-KEM was enabled
+            assert_eq!(res, WOLFSSL_SUCCESS as c_int);
+        }
+
+        // Test ML-KEM is available
+        unsafe {
+            // Init WolfSSL
+            let res = wolfSSL_Init();
+            assert_eq!(res, WOLFSSL_SUCCESS as c_int);
+
+            // Set up client method
+            let method = wolfTLSv1_3_client_method();
+
+            // Create context
+            let context = wolfSSL_CTX_new(method);
+
+            // Create new SSL stream
+            let ssl = wolfSSL_new(context);
+
+            // Enable ML-KEM
+            let res = wolfSSL_UseKeyShare(ssl, WOLFSSL_P521_ML_KEM_1024.try_into().unwrap());
+
+            // Check that ML-KEM was enabled
             assert_eq!(res, WOLFSSL_SUCCESS as c_int);
         }
     }
