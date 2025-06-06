12343f1b96f4672e7eb07dfe15295d577d980474
Max Li <max.li@kape.com>
2025-06-06 16:44:25 +0800

    wolfssl: update configure options for PQC
    the configure.ac file has been updated in WolfSSL 5.8.0.
    The PQC configure options are updated accordingly.
    See: https://github.com/wolfSSL/wolfssl/blob/v5.8.0-stable/configure.ac#L1448-L1559

--- wolfssl-sys/build.rs
+++ wolfssl-sys/build.rs
@@ -140,11 +140,11 @@ fn build_wolfssl(wolfssl_src: &Path) -> PathBuf {
 
     if cfg!(feature = "postquantum") {
         let flags = if cfg!(feature = "kyber_only") {
-            "all,original"
+            "yes,kyber"
         } else {
-            "all,original,ml-kem"
+            "all"
         };
-        // Enable Kyber
+        // Enable Kyber/ML-KEM
         conf.enable("kyber", Some(flags))
             // SHA3 is needed for using WolfSSL's implementation of Kyber/ML-KEM
             .enable("sha3", None);
